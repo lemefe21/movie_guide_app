4263bb8decc39ac107db2901d54818efe0f67b0e
felleme <felleme@prestadores.amil.com.br>
2019-02-13 19:28:03 -0200

    initiating the creation of the MovieSyncIntentService to implement the background tasks

---
+++ app/src/main/java/com/leme/movieguideapp/sync/MovieSyncIntentService.java
@@ -0,0 +1,17 @@
+package com.leme.movieguideapp.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class MovieSyncIntentService extends IntentService {
+
+    public MovieSyncIntentService() {
+        super("MovieSyncIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
